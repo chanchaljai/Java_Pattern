f0508ca89eddb2247da059604030d40786726460
chanchaljai <chanchal123jaiswal@gmail.com>
2024-03-14 11:27:01 +0530

    Half pyramid No.

--- pattern.java
+++ pattern.java
@@ -1,6 +1,12 @@
 class pattern {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       int n=5;
+       for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+       }
     }
 }
