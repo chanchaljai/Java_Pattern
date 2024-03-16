06c3057e516e2f4fca93d9d260118d96cb63ca03
chanchaljai <chanchal123jaiswal@gmail.com>
2024-03-16 09:25:45 +0530

    pattern2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Java_Pattern
 pattern code store <br> learning purpose
+<br> add all java pattern Question
--- starPattern2.java
+++ starPattern2.java
@@ -1,11 +1,14 @@
-public class starPattern1 {
-
+public class starPattern2{
     public static void main(String[] args) {
         int n=4;
         int m=5;
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                System.out.print("*");
+                if(i==1 ||i==n ||j==1 ||j==m){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
